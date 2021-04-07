df48fc1cc8b3c6c83f4be868e89966af84665986
tiagogandarez <gandarez.t@gmail.com>
2021-04-07 10:12:26 +0100

    Add Domain.Core and Implementation of InMemoryBus

---
+++ CleanArch/CleanArch.Domain.Core/Events/Message.cs
@@ -0,0 +1,14 @@
+using MediatR;
+
+namespace CleanArch.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessageType { get; protected set; }
+
+        public Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
